fbd307b5dc2928c6138fa53ed71e4bd277fa5300
admin <admin@admin.ru>
2023-06-18 17:36:36 +0300

    /

--- src/test/java/BrowserSettings.java
+++ src/test/java/BrowserSettings.java
@@ -3,6 +3,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
