45a582c50ae4a3791e4d840bbaca56ac8293ba3e
admin <admin@admin.ru>
2023-06-18 17:04:37 +0300

    /

--- src/test/java/BrowserSettings.java
+++ src/test/java/BrowserSettings.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Configuration;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,26 +13,21 @@ public class BrowserSettings {
 
     public void startBrowser() {
 
-        Configuration.timeout = 6000;
-        Configuration.browser = "chrome";
         //Configuration.browser="opera";
         //Configuration.browser = "edge";
         //Configuration.browser="firefox";
+        WebDriverManager.chromedriver().driverVersion("85").setup();
+        System.setProperty("chromeoptions.args", "--no-sandbox");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Al2\\IdeaProjects\\ui_tests\\src\\test\\resources\\chromedriver.exe");
+        System.setProperty("selenide.browser","chrome");
         ChromeOptions options = new ChromeOptions();
-        options.setBinary("src/test/resources/chromedriver.exe");
-        options.addArguments("--ignore-certificate-errors");
-        options.addArguments("--test-type");
-        options.addArguments("test-type");
-        options.addArguments("start-maximized");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--enable-precise-memory-info");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--disable-default-apps");
-        options.addArguments("test-type=browser");
-        options.addArguments("--incognito");
-        options.addArguments("--no-sandbox");
+        options.addArguments("selenide.browser", "chrome");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+        Configuration.headless = true;
+        Configuration.timeout = 6000;
+        Configuration.browser = "chrome";
         Configuration.browserCapabilities = capabilities;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
     }
